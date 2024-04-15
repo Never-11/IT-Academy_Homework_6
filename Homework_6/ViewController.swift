ad3f4e891106d00cb8413c09b7b3eeafd5b8f0e6
Andrey Neverovskiy <never111194@gmail.com>
2024-04-15 17:24:33 +0300

    Initial project

---
+++ Homework_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_6
+//
+//  Created by Андрей Неверовский on 15.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
