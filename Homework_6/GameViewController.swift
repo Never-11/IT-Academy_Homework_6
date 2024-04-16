9796123d35eda64fcb02125fabfbc6d1b10c0a32
Andrey Neverovskiy <never111194@gmail.com>
2024-04-16 15:03:41 +0300

    XX-2. adding a second screen, adding a file to the screen

---
+++ Homework_6/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondScreen.swift
+//  Homework_6
+//
+//  Created by Андрей Неверовский on 16.04.24.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
